9109a2b5c9caf2eb8b969c89ef43ccf3bef280d9
Abhijith <hrabhijith@gmail.com>
2019-05-05 17:51:28 +0530

    deployment change

--- finalproject.py
+++ finalproject.py
@@ -682,7 +682,5 @@ def fbconnect():
 
 
 # Server host listening starts
-if __name__ == '__main__':
-    app.secret_key = 'super_key'
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
